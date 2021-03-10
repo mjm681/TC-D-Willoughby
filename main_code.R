affa9e6f667cb57a28cc52186b4cf9c86b27e81f
mjm681 <meganjmunro3@gmail.com>
2021-03-10 17:14:55 +0000

    Consolidating to main code

---
+++ main_code.R
@@ -0,0 +1,101 @@
+##### Setting WD #####
+
+setwd("~/TC-D-Willoughby")
+
+##### Headings of Data #####
+
+colnames_101 <- c("Animal type", "Gender", "Animal name", "I&R number",
+                  "Birth date", "Group number", "Location number", 
+                  "Population number")
+
+colnames_102 <- c("Animal number", "Tag number", "Animal name", "I&R number",
+                  "Birth date", "Group number","Dam I&R number",
+                  "Sire I&R number")
+
+colnames_3a02 <- c("Daily date", "Animal number", "Daily production",
+                   "Daily conductivity", "Daily milk time")
+
+colnames_3a06 <- c("Animal number", "I&R number", "Rec. timestamp milk",
+                   "Rec. EOM", "Rec. production", "Rec. frame number",
+                   "Rec. botle number", "24-H production")
+
+colnames_3a07 <- c("Animal number", "Group", "Lactation number",
+                   "Current lactation days", "Average 24-H production",
+                   "24-H production", "Average milkings", "Last event")
+
+
+colnames_3a08 <- c("Animal name", "Animal number", "Lactation", 
+                   "Lactation days in milk", "Lactation production",
+                   "Lactation day 100", "Lactation day 305", 
+                   "Lactation fat", "Lactation protein", "Lactation cellcount")
+
+colnames_3a10 <- c("Animal number", "Tag number", "Group number",
+                   "Rec. timestamp milk", "Rec. production", 
+                   "Rec. conductivity", "Rec. duration", "Rec. location index")
+
+colnames_3a17 <- c("Animal number", "Group number", "Tag number", 
+                   "Lactation number", "Current lactation days",
+                   "Average 24-H production", "24-H production", 
+                   "Last recorded milking", "Hours last milking")
+
+colnames_3b01 <- c("Animal number", "Group number", "Lactation number",
+                   "Current lactation days", "Average 24-H production",
+                   "Dev. 24-H production", "Average 24-H conductivity",
+                   "Dev. 24-H conductivity")
+
+colnames_3b02 <- c("Daily date", "Animal number", "Daily number OK milkings",
+                   "Daily number YCO milkings", "Daily number failed milkings",
+                   "Daily production", "Daily milk time", "Daily conductivity")
+
+colnames_3b05 <- c("Animal number", "Milking enabled", "Milking regime", 
+                   "Milk OK %", "Milk fail %", "Average 24-H production",
+                   "Manual production rate Y/N","Manual production rate",
+                   "Average 24-H production rate", "Target yield", 
+                   "Target milkings", "Samples take")
+
+colnames_3b06 <- c("Animal number", "Animal name", "Lactation",
+                   "Lactation days in milk", "Lactation production",
+                   "Lactation day 100", "Lactation day 305",
+                   "Lactation fat", "Lactation protein", 
+                   "Lactation cellcount")
+
+colnames_3b07 <- c("Animal number", "Animal name", "Group number",
+                   "Lactation number", "Current lactation days",
+                   "24-H production", "Average 24-H production",
+                   "Sire I&R number")
+
+colnames_3a08 <- c("Animal number", "Group number", "Rec. timestamp milk",
+                   "Rec. conductivity", "Rec. duration", "Rec. location index",
+                   "Rec. EOM")
+
+colnames_401 <- c("Animal number", "Group number", "Current lactation days",
+                  "Average 24-H production", "24-H production", "Hours last OK milking",
+                  "Last recorded milking", "Last recorded EOM",
+                  "Last recorded production", "Average 24-H production rate",
+                  "Target yield", "Average milkings", "Target yield reached %")
+
+colnames_402 <- c("Daily date", "Animal number", "Daily number OK milkings",
+                  "Daily number YCO milkings", "Daily number failed milkings",
+                  "Daily production", "Daily milk time", "Daily conductivity")
+
+
+colnames_403 <- c("Animal number", "Daily date", "Average 24-H production",
+                  "Target yield", "Average milkings", "Target milkings",
+                  "Daily number milkings", "Daily number OK milkings",
+                  "Daily number failed milkings", "Daily number YCO milkings",
+                  "Attachment ease")
+
+colnames_404 <- c("Animal number", "Average 24-H production", "Enable quarter",
+                  "Manual assistance", "Number of treatments", "Long teats",
+                  "Back flush", "Disinfect udder", "Milk LF", "Milk LR",
+                  "Milk RF", "Milk RR")
+
+colnames_405 <- c("Rec. location index", "Rec. production","Rec. duration", 
+                  "Rec. EOM", "Rec. timestamp milk")
+
+colnames_406 <- c("Animal number", "Average 24-H production", "Rec. timestamp milk",
+                  "Rec. location index", "Rec. production", "Rec. conductivity",
+                  "Rec. duration", "Rec. EOM")
+
+##### Reading in the data #####
+
