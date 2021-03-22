37128602e971becfa2a835280aa5023937ce4c80
mjm681 <meganjmunro3@gmail.com>
2021-03-22 15:08:04 +0000

    Making vector of all column headings

--- main_code.R
+++ main_code.R
@@ -17,7 +17,7 @@ colnames_3a02 <- c("Daily date", "Animal number", "Daily production",
 
 colnames_3a06 <- c("Animal number", "I&R number", "Rec. timestamp milk",
                    "Rec. EOM", "Rec. production", "Rec. frame number",
-                   "Rec. botle number", "24-H production")
+                   "Rec. bottle number", "24-H production")
 
 colnames_3a07 <- c("Animal number", "Group", "Lactation number",
                    "Current lactation days", "Average 24-H production",
@@ -64,7 +64,7 @@ colnames_3b07 <- c("Animal number", "Animal name", "Group number",
                    "24-H production", "Average 24-H production",
                    "Sire I&R number")
 
-colnames_3a08 <- c("Animal number", "Group number", "Rec. timestamp milk",
+colnames_3b08 <- c("Animal number", "Group number", "Rec. timestamp milk",
                    "Rec. conductivity", "Rec. duration", "Rec. location index",
                    "Rec. EOM")
 
@@ -97,5 +97,219 @@ colnames_406 <- c("Animal number", "Average 24-H production", "Rec. timestamp mi
                   "Rec. location index", "Rec. production", "Rec. conductivity",
                   "Rec. duration", "Rec. EOM")
 
-##### Reading in the data #####
+colnames_407 <- c("Rec. location index", "Rec. timestamp quarter", "Rec. LF deadtime", 
+                  "Rec. LR deadtime", "Rec. RF deadtime", "Rec. RR deadtime", 
+                  "Rec. LF milktime", "Rec. LR milktime", "Rec. RF milktime", 
+                  "Rec. RR milktime")
 
+colnames_408 <- c("Daily date", "Animal number", "Daily production", "Daily number milkings",
+                  "Daily number OK milkings","Daily number failed milkings", "Daily number YCO milkings")
+
+colnames_409 <- c("Animal number", "Current lactation days", "Average 24-H production",
+                  "Target yield", "Average milkings", "Target milkings",
+                  "YCO milkings %", "Failed milkings %", "Attachment ease")
+
+colnames_410 <- c("Animal number", "Group number", "Current lactation days", "Average 24-H production",
+                  "24-H production", "Hours last OK milking", "Last rec. milking", "Last rec. production",
+                  "Last rec. EOM", "Average 24-H production rate", "Target yield")
+
+colnames_412 <- c("Animal number", "Teat EcDr LF", "Rec. LF FQ cond. dev.", "Teat EcDr LR", "Rec. LR FQ cond. dev."
+                  , "Teat EcDr RF", "Rec. RF FQ cond. dev.", "Teat EcDr RR", "Rec. RR FQ cond. dev.",
+                  "Rec. FQ mean max.")
+
+colnames_5a01 <- c("Animal number", "Current lactation days", "Average 24-H production", "C. Ytd All. OOP. A",
+                   "C. Ytd Uneaten OOP. B", "C. Ytd All. OOP. B", "C. Ytd Uneaten OOP. B", "C. Noof meals",
+                   "C. Wait time")
+# CHECK THIS ^^^ IS RIGHT, NOT 413
+
+colnames_5a02 <- c("Animal number", "Group number", "Current lactation days", "Average 24-H production",
+                   "C. Feeding Enabled", "C. Calc Alg.", "C. Allowance feed A", "C. Allowance feed B",
+                   "C. Noof meals", "C. Wait time", "C. Eating rate", "C. Automatic meal calculation")
+
+colnames_5a03 <- c("Animal number", "Current lactation days", "Lactation number", "Average 24-H production",
+                   "C. Calc Alg.", "C. Allowance feed A", "C. Allowance feed B", "Expected dry", "Expected calving")
+
+colnames_5a04 <- c("Daily date", "Animal number", "Daily production", "C. Daily feed A", "C. Daily Eaten A",
+                   "C. Daily feed B", "C. Daily Eaten B")
+
+colnames_5a05 <- c("C. Rec Loc. Index", "Animal number", "C. Rec. stamp", "C. Rec Eaten A", "C. Rec Eaten B",
+                   "C. Rec Eaten C", "C. Rec Eaten D")
+
+colnames_5a06 <- c("C. Calc Alg.", "Animal number", "Current lactation days", "Average 24-H production",
+                   "Expected calving", "C. Allowance feed A", "C. Target A", "C. Step A", "C. Allowance feed B",
+                   "C. Target B", "C. Step B")
+
+colnames_5a07 <- c("C. Calc Alg.", "C. Feed Curve", "Animal number", "Current lactation days", "Average 24-H production",
+                   "Expected calving", "C. Curve startdate", "C. Allowance feed A", "C. Curve A", "C. Allowance feed B",
+                   "C. Curve B")
+
+
+all_colnames <- list(colnames_101, colnames_102, colnames_3a02, colnames_3a06, colnames_3a07, colnames_3a08,
+                    colnames_3a10, colnames_3a17, colnames_3b01, colnames_3b02, colnames_3b05, colnames_3b06,
+                    colnames_3b07, colnames_3b08, colnames_401, colnames_402, colnames_403, colnames_404, 
+                    colnames_405, colnames_406, colnames_407, colnames_408, colnames_409, colnames_410)
+
+##### 
+
+headings <- c("Animal name",
+              "Animal number",
+              "Animal type",
+              "Attachment ease",
+              "Average milkings",
+              "Average 24-H conductivity",
+              "Average 24-H production",
+              "Average 24-H production rate",
+              "Back flush",
+              "Birth date",
+              "C. Allowance feed A",
+              "C. Allowance feed B",
+              "C. Automatic meal calculation",
+              "C. Calc Alg.",
+              "C. Curve A",
+              "C. Curve B",
+              "C. Curve startdate",
+              "C. Daily Eaten A",
+              "C. Daily Feed A",
+              "C. Daily Eaten B",
+              "C. Daily Feed B", 
+              "C. Eating rate",
+              "C. Feed Curve",
+              "C. Feeding Enabled",
+              "C. Noof meals",
+              "C. Rec Eaten A",
+              "C. Rec Eaten B",
+              "C. Rec Eaten C",
+              "C. Rec Eaten D",
+              "C. Rec Loc. Index",
+              "C. Rec. Stamp",
+              "C. Step A",
+              "C. Step B",
+              "C. Target A",
+              "C. Target B",
+              "C. Wait time",
+              "C. Ytd All. OOP. A",
+              "C. Ytd All. OOP. B", 
+              "C. Ytd Uneaten OOP. A",
+              "C. Ytd Uneaten OOP. B", 
+              "Current lactation days",
+              "Daily conductivity",
+              "Daily date",
+              "Daily milk time",
+              "Daily number failed milkings",
+              "Daily number milkings",
+              "Daily number OK milkings",
+              "Daily number YCO milkings", 
+              "Daily production",
+              "Dam I&R number",
+              "Dev. 24-H conductivity",
+              "Dev. 24-H production",
+              "Disinfect udder",
+              "Enable quarter",
+              "Expected calving",
+              "Expected dry",
+              "Failed milkings %",
+              "Gender",
+              "Group",
+              "Group number",
+              "Hours last milking",
+              "Hours last OK milking",
+              "I&R number",
+              "Lactation",
+              "Lactation cellcount",
+              "Lactation days in milk",
+              "Lactation day 100",
+              "Lactation day 305",
+              "Lactation fat",
+              "Lactation number",
+              "Lactation production",
+              "Lactation protein",
+              "Last event",
+              "Last recorded EOM",
+              "Last recorded milking",
+              "Last recorded production",
+              "Location number", 
+              "Long teats",
+              "Manual assistance",
+              "Manual production rate",
+              "Manual production rate Y/N",
+              "Milk fail %",
+              "Milk LF",
+              "Milk LR",
+              "Milk OK %",
+              "Milk RF",
+              "Milk RR",
+              "Milking enabled", 
+              "Milking regime", 
+              "Number of treatments",
+              "Population number",
+              "Samples take",
+              "Sire I&R number",
+              "Tag number",
+              "Target milkings",
+              "Target yield",
+              "Target yield reached %",
+              "Teat EcDr LF",
+              "Teat EcDr LR",
+              "Teat EcDr RF",
+              "Teat EcDr RR",
+              "Rec. bottle number",
+              "Rec. conductivity",
+              "Rec. duration",
+              "Rec. EOM",
+              "Rec. frame number",
+              "Rec. FQ mean max.",
+              "Rec. LF deadtime",
+              "Rec. LF FQ cond. dev.",
+              "Rec. LR FQ cond. dev.",
+              "Rec. LF milktime",
+              "Rec. location index",
+              "Rec. LR deadtime", 
+              "Rec. LR milktime",
+              "Rec. production",
+              "Rec. RF deadtime", 
+              "Rec. RF FQ cond. dev.",
+              "Rec. RF milktime",
+              "Rec. RR deadtime",
+              "Rec. RR FQ cond. dev.",
+              "Rec. RR milktime",
+              "Rec. timestamp milk",
+              "Rec. timestamp quarter",
+              "YCO milkings %",
+              "24-H production"
+)
+
+
+
+
+# colnames_5a01 # CHECK THIS ^^^ IS RIGHT, NOT 413
+
+
+
+##### Reading in the data and assigning column names #####
+
+setwd("~/TC-D-Willoughby/Report")
+
+file_list <- list.files(path = ".")
+
+### Inital attempt ###
+#for (i in 1:length(file_list)) {
+#  nam <- paste(file_list[i], sep = "")
+#  nam <- sub(".txt", "", nam)
+#  file <- read.csv(file_list[i], header = F, skip = 1, na.strings = '-')
+#  colnames(file) <- all_colnames[i]
+#  assign(nam, file)
+#  rm(file)
+#}
+
+df_list <- list()
+
+for ( i in seq_along(file_list)) {
+  df_list[[i]] <- read.csv(file_list[i], header = F, skip = 1, na.strings = '-')
+}
+
+
+file_list_no_txt <- sub(".txt", "", file_list)
+
+
+colnames(all_animals_df) <- colnames_101
+colnames(all_milkcows_df) <- colnames_102
