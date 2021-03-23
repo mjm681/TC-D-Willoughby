77289c630968866b139ec39140c344ffb66105ea
mjm681 <meganjmunro3@gmail.com>
2021-03-23 12:25:59 +0000

    Reading in data by file name and checking if they are empty

--- main_code.R
+++ main_code.R
@@ -1,8 +1,199 @@
-##### Setting WD #####
+##### Setting Project Woking Directory #####
 
 setwd("~/TC-D-Willoughby")
 
-##### Headings of Data #####
+##### All Possible Headings of Data #####
+
+
+# All Possible Headings of Data in Alphabetical Order
+
+headings <- list("Activity regime",
+                 "Animal name",
+                 "Animal number",
+                 "Animal type",
+                 "Attachment ease",
+                 "Average milkings",
+                 "Average 24-H conductivity",
+                 "Average 24-H production",
+                 "Average 24-H production rate",
+                 "Back flush",
+                 "Birth date",
+                 "C. Allowance feed A",
+                 "C. Allowance feed B",
+                 "C. Automatic meal calculation",
+                 "C. Bonus A",
+                 "C. Bonus B",
+                 "C. Bonus Days",
+                 "C. Calc Alg.",
+                 "C. Current Lactation All A",
+                 "C. Current Lactation All B",
+                 "C. Current Lactation Eaten A",
+                 "C. Current Lactation Eaten B",
+                 "C. Curve A",
+                 "C. Curve B",
+                 "C. Curve startdate",
+                 "C. Daily Eaten A",
+                 "C. Daily Feed A",
+                 "C. Daily Eaten B",
+                 "C. Daily Feed B", 
+                 "C. Dist. Alg.",
+                 "C. Eating rate",
+                 "C. Feed Curve",
+                 "C. Feed Rest",
+                 "C. Feeding Enabled",
+                 "C. KgM+ A",
+                 "C. KgM+ B",
+                 "C. KgM+ Apply",
+                 "C. KgM+ Curve",
+                 "C. Lactation Eaten A",
+                 "C. Lactation Eaten B",
+                 "C. Last Fed",
+                 "C. Max Meal",
+                 "C. Min Meal",
+                 "C. Noof meals",
+                 "C. Rec Eaten A",
+                 "C. Rec Eaten B",
+                 "C. Rec Eaten C",
+                 "C. Rec Eaten D",
+                 "C. Rec Loc. Index",
+                 "C. Rec. Stamp",
+                 "C. Step A",
+                 "C. Step B",
+                 "C. Target A",
+                 "C. Target B",
+                 "C. Today All. OOP A",
+                 "C. Today All. OOP B",
+                 "C. Today Uneaten OOP A",
+                 "C. Today Uneaten OOP B",
+                 "C. Wait time",
+                 "C. Ytd All. OOP. A",
+                 "C. Ytd All. OOP. B", 
+                 "C. Ytd Uneaten OOP. A",
+                 "C. Ytd Uneaten OOP. B", 
+                 "C. 24-H All. OOP. A",
+                 "C. 24-H All. OOP. B",
+                 "C. 24-H Eaten OOP. A",
+                 "C. 24-H Eaten OOP. B",
+                 "C. 24-H Uneaten OOP. A",
+                 "C. 24-H Uneaten OOP. B",
+                 "Current lactation day 100",
+                 "Current lactation day 305",
+                 "Current lactation days",
+                 "Current lactation days in milk",
+                 "Current lactation fat",
+                 "Current lactation production",
+                 "Current lactation protein",
+                 "Daily conductivity",
+                 "Daily date",
+                 "Daily milk time",
+                 "Daily number failed milkings",
+                 "Daily number milkings",
+                 "Daily number OK milkings",
+                 "Daily number YCO milkings", 
+                 "Daily production",
+                 "Dam I&R number",
+                 "Dev. eve. activity",
+                 "Dev. eve. production rate",
+                 "Dev. morn. activity",
+                 "Dev. morn. production rate",
+                 "Dev. noon activity",
+                 "Dev. noon production rate",
+                 "Dev. 24-H activity",
+                 "Dev. 24-H conductivity",
+                 "Dev. 24-H production",
+                 "Disinfect udder",
+                 "Enable quarter",
+                 "Expected calving",
+                 "Expected dry",
+                 "Failed milkings %",
+                 "Gender",
+                 "Group",
+                 "Group number",
+                 "Hours last milking",
+                 "Hours last OK milking",
+                 "I&R number",
+                 "Lactation",
+                 "Lactation cellcount",
+                 "Lactation days in milk",
+                 "Lactation day 100",
+                 "Lactation day 305",
+                 "Lactation fat",
+                 "Lactation number",
+                 "Lactation production",
+                 "Lactation protein",
+                 "Last calving date",
+                 "Last dry off",
+                 "Last event",
+                 "Last oestrus",
+                 "Last pregnancy diagnosis negative",
+                 "Last pregnancy diagnosis positive",
+                 "Last recorded EOM",
+                 "Last recorded milking",
+                 "Last recorded production",
+                 "Last service",
+                 "Location number", 
+                 "Long teats",
+                 "Manual assistance",
+                 "Manual production rate",
+                 "Manual production rate Y/N",
+                 "Milk fail %",
+                 "Milk LF",
+                 "Milk LR",
+                 "Milk OK %",
+                 "Milk RF",
+                 "Milk RR",
+                 "Milking enabled", 
+                 "Milking regime", 
+                 "Number of treatments",
+                 "Population number",
+                 "Samples take",
+                 "Sire I&R number",
+                 "Tag number",
+                 "Target milkings",
+                 "Target yield",
+                 "Target yield reached %",
+                 "Teat EcDr LF",
+                 "Teat EcDr LR",
+                 "Teat EcDr RF",
+                 "Teat EcDr RR",
+                 "Rec. activity",
+                 "Rec. bottle number",
+                 "Rec. conductivity",
+                 "Rec. duration",
+                 "Rec. EOM",
+                 "Rec. frame number",
+                 "Rec. FQ mean max.",
+                 "Rec. LF deadtime",
+                 "Rec. LF FQ cond. dev.",
+                 "Rec. LR FQ cond. dev.",
+                 "Rec. LF milktime",
+                 "Rec. location index",
+                 "Rec. LR deadtime", 
+                 "Rec. LR milktime",
+                 "Rec. production",
+                 "Rec. RF deadtime", 
+                 "Rec. RF FQ cond. dev.",
+                 "Rec. RF milktime",
+                 "Rec. RR deadtime",
+                 "Rec. RR FQ cond. dev.",
+                 "Rec. RR milktime",
+                 "Rec. timestamp activity",
+                 "Rec. timestamp milk",
+                 "Rec. timestamp quarter",
+                 "YCO milkings %",
+                 "24-H production"
+)
+
+# colnames_5a01 # CHECK THIS ^^^ IS RIGHT, NOT 413
+
+# Checking the headings are alphabetical
+
+isFALSE(is.unsorted(headings, strictly = T))
+
+
+
+#####
+
 
 colnames_101 <- c("Animal type", "Gender", "Animal name", "I&R number",
                   "Birth date", "Group number", "Location number", 
@@ -144,171 +335,47 @@ colnames_5a07 <- c("C. Calc Alg.", "C. Feed Curve", "Animal number", "Current la
                    "C. Curve B")
 
 
-all_colnames <- list(colnames_101, colnames_102, colnames_3a02, colnames_3a06, colnames_3a07, colnames_3a08,
-                    colnames_3a10, colnames_3a17, colnames_3b01, colnames_3b02, colnames_3b05, colnames_3b06,
-                    colnames_3b07, colnames_3b08, colnames_401, colnames_402, colnames_403, colnames_404, 
-                    colnames_405, colnames_406, colnames_407, colnames_408, colnames_409, colnames_410)
-
-##### 
-
-headings <- c("Animal name",
-              "Animal number",
-              "Animal type",
-              "Attachment ease",
-              "Average milkings",
-              "Average 24-H conductivity",
-              "Average 24-H production",
-              "Average 24-H production rate",
-              "Back flush",
-              "Birth date",
-              "C. Allowance feed A",
-              "C. Allowance feed B",
-              "C. Automatic meal calculation",
-              "C. Calc Alg.",
-              "C. Curve A",
-              "C. Curve B",
-              "C. Curve startdate",
-              "C. Daily Eaten A",
-              "C. Daily Feed A",
-              "C. Daily Eaten B",
-              "C. Daily Feed B", 
-              "C. Eating rate",
-              "C. Feed Curve",
-              "C. Feeding Enabled",
-              "C. Noof meals",
-              "C. Rec Eaten A",
-              "C. Rec Eaten B",
-              "C. Rec Eaten C",
-              "C. Rec Eaten D",
-              "C. Rec Loc. Index",
-              "C. Rec. Stamp",
-              "C. Step A",
-              "C. Step B",
-              "C. Target A",
-              "C. Target B",
-              "C. Wait time",
-              "C. Ytd All. OOP. A",
-              "C. Ytd All. OOP. B", 
-              "C. Ytd Uneaten OOP. A",
-              "C. Ytd Uneaten OOP. B", 
-              "Current lactation days",
-              "Daily conductivity",
-              "Daily date",
-              "Daily milk time",
-              "Daily number failed milkings",
-              "Daily number milkings",
-              "Daily number OK milkings",
-              "Daily number YCO milkings", 
-              "Daily production",
-              "Dam I&R number",
-              "Dev. 24-H conductivity",
-              "Dev. 24-H production",
-              "Disinfect udder",
-              "Enable quarter",
-              "Expected calving",
-              "Expected dry",
-              "Failed milkings %",
-              "Gender",
-              "Group",
-              "Group number",
-              "Hours last milking",
-              "Hours last OK milking",
-              "I&R number",
-              "Lactation",
-              "Lactation cellcount",
-              "Lactation days in milk",
-              "Lactation day 100",
-              "Lactation day 305",
-              "Lactation fat",
-              "Lactation number",
-              "Lactation production",
-              "Lactation protein",
-              "Last event",
-              "Last recorded EOM",
-              "Last recorded milking",
-              "Last recorded production",
-              "Location number", 
-              "Long teats",
-              "Manual assistance",
-              "Manual production rate",
-              "Manual production rate Y/N",
-              "Milk fail %",
-              "Milk LF",
-              "Milk LR",
-              "Milk OK %",
-              "Milk RF",
-              "Milk RR",
-              "Milking enabled", 
-              "Milking regime", 
-              "Number of treatments",
-              "Population number",
-              "Samples take",
-              "Sire I&R number",
-              "Tag number",
-              "Target milkings",
-              "Target yield",
-              "Target yield reached %",
-              "Teat EcDr LF",
-              "Teat EcDr LR",
-              "Teat EcDr RF",
-              "Teat EcDr RR",
-              "Rec. bottle number",
-              "Rec. conductivity",
-              "Rec. duration",
-              "Rec. EOM",
-              "Rec. frame number",
-              "Rec. FQ mean max.",
-              "Rec. LF deadtime",
-              "Rec. LF FQ cond. dev.",
-              "Rec. LR FQ cond. dev.",
-              "Rec. LF milktime",
-              "Rec. location index",
-              "Rec. LR deadtime", 
-              "Rec. LR milktime",
-              "Rec. production",
-              "Rec. RF deadtime", 
-              "Rec. RF FQ cond. dev.",
-              "Rec. RF milktime",
-              "Rec. RR deadtime",
-              "Rec. RR FQ cond. dev.",
-              "Rec. RR milktime",
-              "Rec. timestamp milk",
-              "Rec. timestamp quarter",
-              "YCO milkings %",
-              "24-H production"
-)
-
-
-
-
-# colnames_5a01 # CHECK THIS ^^^ IS RIGHT, NOT 413
-
-
 
 ##### Reading in the data and assigning column names #####
 
+# Setting working data directory
 setwd("~/TC-D-Willoughby/Report")
 
+# List all files in directory
 file_list <- list.files(path = ".")
 
-### Inital attempt ###
-#for (i in 1:length(file_list)) {
-#  nam <- paste(file_list[i], sep = "")
-#  nam <- sub(".txt", "", nam)
-#  file <- read.csv(file_list[i], header = F, skip = 1, na.strings = '-')
-#  colnames(file) <- all_colnames[i]
-#  assign(nam, file)
-#  rm(file)
-#}
-
+# Make empty list to read the data into
 df_list <- list()
 
-for ( i in seq_along(file_list)) {
-  df_list[[i]] <- read.csv(file_list[i], header = F, skip = 1, na.strings = '-')
+# Remove .txt from file names
+file_list_no_txt <- sub(".txt", "", file_list)
+
+### Read in the data from the directory
+## If there is no data, return an error
+for ( i in seq_along(file_list_no_txt)) {
+  df_list[[i]] <- read.csv(file_list[i], header = F, na.strings = '-')
+  if (nrow(df_list[[i]]) != 1) {
+    df_list[[i]] <- read.csv(file_list[i], header = F, skip = 1, na.strings = '-')
+  }
+  else {
+    print(paste("Error:", file_list_no_txt[i], "file has no data"))
+  }
+}
+
+# Make empty list to store the columns
+all_colnames <- list()
+
+# Fill column vector with the filenames
+for ( i in seq_along(file_list_no_txt) ) {
+  all_colnames[i] <- paste("colnames_", file_list_no_txt[i])
 }
 
 
-file_list_no_txt <- sub(".txt", "", file_list)
+
+for (i in 1:file_list_no_txt) {
+  colnames <- headings[[i]]
+}
+
 
 
 colnames(all_animals_df) <- colnames_101
